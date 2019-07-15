06f20d138c0c2f37dade5f4b2eee3c8e5e34fc73
Lester Hurtado <lester@hurtado.ph>
2019-07-15 16:42:54 +0800

    add ordinal and tara

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -8,11 +8,11 @@ if (!function_exists('validate_mobile')) {
         ]);
 
         return ! $validator->fails();
-    } 
+    }
 }
 
 if (!function_exists('suffixate_filename')) {
-	
+
 	function suffixate_filename($filename, $suffix, $delimiter = '')
 	{
 		$filename_no_extension = preg_replace('/.[^.]*$/', '', $filename);
@@ -20,4 +20,45 @@ if (!function_exists('suffixate_filename')) {
 
 		return "{$filename_no_extension}{$delimiter}{$suffix}.{$extension}";
 	}
+}
+
+if (!function_exists('ordinal')) {
+
+    function ordinal($number, $locale ='en_US')
+    {
+        $nf = new \NumberFormatter($locale, NumberFormatter::ORDINAL);
+
+        return $nf->format($number);
+    }
+}
+
+//to be used for TallyMark.ttf
+if (!function_exists('tara')) {
+
+    function tara($value)
+    {
+        $multiples = intdiv($value,5);
+        $remainder = $value % 5;
+
+        $retval = '';
+        for ($i=1; $i<= $multiples; $i++) {
+            $retval .= 'e';
+        }
+        switch ($remainder) {
+            case 1:
+                $retval .= 'a';
+                break;
+            case 2:
+                $retval .= 'b';
+                break;
+            case 3:
+                $retval .= 'c';
+                break;
+            case 4:
+                $retval .= 'd';
+                break;
+        }
+
+        return $retval;
+    }
 }
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -14,7 +14,7 @@ class UtilsTest extends TestCase
 		$this->assertTrue(validate_mobile('9173011987'));
 		$this->assertFalse(validate_mobile('023011987'));
 		$this->assertFalse(validate_mobile('3011987'));
-	} 
+	}
 
 	/** @test */
 	public function suffixate_filename_works()
@@ -24,4 +24,34 @@ class UtilsTest extends TestCase
 		$this->assertEquals('test-003.jpg', suffixate_filename('test.jpg', '003', '-'));
 		$this->assertEquals('test004.jpg', suffixate_filename('test.jpg', '004'));
 	}
+
+	/** @test */
+	public function ordinal_works()
+    {
+        $this->assertEquals('1st', ordinal(1));
+        $this->assertEquals('2nd', ordinal(2));
+        $this->assertEquals('3rd', ordinal(3));
+        $this->assertEquals('4th', ordinal(4));
+        $this->assertEquals('5th', ordinal(5));
+        $this->assertEquals('6th', ordinal(6));
+        $this->assertEquals('7th', ordinal(7));
+        $this->assertEquals('8th', ordinal(8));
+        $this->assertEquals('9th', ordinal(9));
+        $this->assertEquals('10th', ordinal(10));
+    }
+
+    /** @test */
+    public function tara_works()
+    {
+        $this->assertEquals('a', tara(1));
+        $this->assertEquals('b', tara(2));
+        $this->assertEquals('c', tara(3));
+        $this->assertEquals('d', tara(4));
+        $this->assertEquals('e', tara(5));
+        $this->assertEquals('ea', tara(6));
+        $this->assertEquals('eb', tara(7));
+        $this->assertEquals('ec', tara(8));
+        $this->assertEquals('ed', tara(9));
+        $this->assertEquals('ee', tara(10));
+    }
 }
